4b0975d209d9f97199f148fc09883fdf28d7ec72
LMJW <heysuperming@gmail.com>
2019-02-09 10:19:26 +1100

    using dynamic programming

--- minmax/lc375.cpp
+++ minmax/lc375.cpp
@@ -5,6 +5,25 @@
 
 using namespace std;
 
+struct point {
+    int x;
+    int y;
+    point() : x(0), y(0) {}
+    point(int a, int b) : x(a), y(b) {}
+    friend bool operator==(const point& p1, const point& p2) {
+        return p1.x == p2.x && p1.y == p2.y;
+    }
+};
+
+template <>
+struct hash<point> {
+    size_t operator()(point const& p) const noexcept {
+        size_t h1 = hash<int>{}(p.x);
+        size_t h2 = hash<int>{}(p.y);
+        return h1 ^ (h2 << 1);
+    }
+};
+
 class Solution {
 public:
     /**
@@ -12,27 +31,38 @@ public:
      * we can only use brute force to pice pivot
      * The
      */
-    int getMoneyAmount(int n) { return divideconq(1, n); }
+    int getMoneyAmount(int n) {
+        unordered_map<point, int> dp;
+        return divideconq(dp, 1, n);
+    }
 
-    int divideconq(int start, int end) {
+    int divideconq(unordered_map<point, int>& dp, int start, int end) {
+        auto p = point(start, end);
+        auto it = dp.find(p);
+        if (it != dp.end()) {
+            return it->second;
+        };
+
         if (start >= end) {
             return 0;
         }
 
         int res = numeric_limits<int>::max();
         for (int i = start; i <= end; ++i) {
-            int left = divideconq(start, i - 1);
-            int right = divideconq(i + 1, end);
+            int left = divideconq(dp, start, i - 1);
+            int right = divideconq(dp, i + 1, end);
             int cur = i + max(left, right);
             if (res > cur) {
                 res = cur;
             }
         }
+        dp.insert({point(start, end), res});
+
         return res;
     }
 };
 
 int main() {
-    int x = Solution().getMoneyAmount(20);
+    int x = Solution().getMoneyAmount(100);
     cout << x << endl;
 }
